dcd466e52980d218340730763c9f28d9c61829eb
YordanYordanov <yordanyordanov0104@gmail.com>
2025-10-13 18:44:48 +0300

    test

---
+++ app/Jobs/TestEmailJob.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Mail;
+
+class TestEmailJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public function handle(): void
+    {
+        Mail::raw('🔥 Test mail from BullWrite queue', function ($m) {
+            $m->to('yordanyordanov0104@gmail.com')
+                ->subject('Test from Laravel Queue ✅');
+        });
+    }
+}
