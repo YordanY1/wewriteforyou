36fa845462afbe48f92c030df85684f79e86038d
YordanYordanov <yordanyordanov0104@gmail.com>
2025-10-18 23:48:38 +0300

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return $this->email === 'yordan@gmail.com';
+        return $this->email === 'bullwrite@gmail.com';
     }
     /**
      * The attributes that are mass assignable.
