e5fffb9c0c65439fe915678a5086bf1b31317cd1
YordanYordanov <yordanyordanov0104@gmail.com>
2026-01-20 18:48:43 +0200

    Update payment.blade.php

--- resources/views/components/layouts/payment.blade.php
+++ resources/views/components/layouts/payment.blade.php
@@ -7,6 +7,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    @if (app()->environment('production'))
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17883089577"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+
+            function gtag() {
+                dataLayer.push(arguments);
+            }
+            gtag('js', new Date());
+            gtag('config', 'AW-17883089577');
+        </script>
+    @endif
+
 </head>
 
 <body class="bg-gray-50 flex items-center justify-center min-h-screen font-sans">
